ada4237c96a7fe86859706b067756a422d394296
Rizwan Mushtaq <magento.guru@yahoo.com>
2023-12-25 00:20:35 +0800

    export for lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,9 +2,9 @@ import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-const lusitana = Lusitana({
+export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
 
-export default lusitana;
+
