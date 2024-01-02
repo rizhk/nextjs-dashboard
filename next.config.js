718bc002fb13bd2949eeef02b5cd744fd2f026fd
Rizwan Mushtaq <magento.guru@yahoo.com>
2024-01-02 21:45:57 +0800

    adding url for images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost'],
-    },
+  images: {
+    domains: [
+      'localhost',
+      'https://lovely-cheese-1cf229bd08.media.strapiapp.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
