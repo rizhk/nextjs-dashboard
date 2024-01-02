8bcafa3f453d75382dd35dd2f10f9830dfe2466b
Rizwan Mushtaq <magento.guru@yahoo.com>
2024-01-03 01:27:36 +0800

    changed value attribute

--- app/ui/actualites/edit-form.tsx
+++ app/ui/actualites/edit-form.tsx
@@ -19,7 +19,7 @@ export default function EditActualityForm({ actuality }: { actuality: any }) {
           id="id"
           name="id"
           type="hidden"
-          defaultValue={actuality.id}
+          value={actuality.id}
           className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
         />
         {/* Invoice Amount */}
